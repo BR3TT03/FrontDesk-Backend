ad7a52d6b01d78df46a0ef017305fe68c3c328bd
BR3TT03 <rockbrett03@gmail.com>
2020-07-03 14:49:29 +0545

    minor adjustments

--- src/main/java/com/example/demo/controller/FrontDeskController.java
+++ src/main/java/com/example/demo/controller/FrontDeskController.java
@@ -62,7 +62,7 @@ public class FrontDeskController {
 	@GetMapping("/totalPages")
 	public int getTotalPages() {
 		int x = 0;
-		float totalPage = clientInfoService.getTotalPages()/10;
+		float totalPage = clientInfoService.getTotalPages();
 		if(totalPage%10!=0) {
 			x = (int)(totalPage/10) + 1;
 			return x;
